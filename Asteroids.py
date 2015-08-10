db2ef4710a135b321586336572ec85d0a5620029
dmitrycx <dkolotsey@gmail.com>
2015-08-10 15:40:00 +0300

    asteroids fixed

--- Asteroids.py
+++ Asteroids.py
@@ -2,7 +2,7 @@
 import simplegui
 import math
 import random
-import time
+
 
 #consts
 WIDTH = 800
@@ -45,42 +45,41 @@ class ImageInfo:
 
     
 # IMAGES & SOUNDS 
-# debris images - debris1_brown.png, debris2_brown.png, debris3_brown.png, debris4_brown.png
-#                 debris1_blue.png, debris2_blue.png, debris3_blue.png, debris4_blue.png, debris_blend.png
+# debris
 debris_info = ImageInfo([320, 240], [640, 480])
-debris_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris2_blue.png")
+debris_image = simplegui.load_image("https://www.dropbox.com/s/64f56rp4tj0xgga/debris2_blue.png?dl=1")
 
-# nebula images - nebula_brown.png, nebula_blue.png
+# nebula images
 nebula_info = ImageInfo([400, 300], [800, 600])
-nebula_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.f2014.png")
+nebula_image = simplegui.load_image("https://www.dropbox.com/s/bx4ddh3rb9d909v/nebula_blue.f2014.png?dl=1")
 
 # splash image
 splash_info = ImageInfo([200, 150], [400, 300])
-splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/splash.png")
+splash_image = simplegui.load_image("https://www.dropbox.com/s/6amsclvy65597s9/splash.png?dl=1")
 
 # ship image
 ship_info = ImageInfo([45, 45], [90, 90], 35)
 ship_info_thrust = ImageInfo([135, 45], [90, 90], 35)
-ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
+ship_image = simplegui.load_image("https://www.dropbox.com/s/sgs1f7sn2wzg5b5/double_ship.png?dl=1")
 
-# missile image - shot1.png, shot2.png, shot3.png
+# missile image
 missile_info = ImageInfo([5,5], [10, 10], 3, 50)
-missile_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/shot2.png")
+missile_image = simplegui.load_image("https://www.dropbox.com/s/m7h3mu393uxvcjg/shot2.png?dl=1")
 
-# asteroid images - asteroid_blue.png, asteroid_brown.png, asteroid_blend.png
+# asteroid images
 asteroid_info = ImageInfo([45, 45], [90, 90], 40)
-asteroid_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blue.png")
+asteroid_image = simplegui.load_image("https://www.dropbox.com/s/gn8n417vfvzrqqs/asteroid_blue.png?dl=1")
 
-# animated explosion - explosion_orange.png, explosion_blue.png, explosion_blue2.png, explosion_alpha.png
+# animated explosion
 explosion_info = ImageInfo([64, 64], [128, 128], 17, 24, True)
-explosion_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/explosion_alpha.png")
+explosion_image = simplegui.load_image("https://www.dropbox.com/s/c1ydyn7d04rq2n3/explosion_alpha.png?dl=1")
 
-# sound assets purchased from sounddogs.com, please do not redistribute
-soundtrack = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/soundtrack.mp3")
-missile_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3")
+# sounds
+soundtrack = simplegui.load_sound("https://www.dropbox.com/s/omk26ogbksw80jg/soundtrack.mp3?dl=1")
+missile_sound = simplegui.load_sound("https://www.dropbox.com/s/s8iplzxxu6wmk8w/missile.mp3?dl=1")
 missile_sound.set_volume(.5)
-ship_thrust_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3")
-explosion_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/explosion.mp3")
+ship_thrust_sound = simplegui.load_sound("https://www.dropbox.com/s/5y7jst6kntl1y2q/thrust.mp3?dl=1")
+explosion_sound = simplegui.load_sound("https://www.dropbox.com/s/tqdbzxuppx2fluj/explosion.mp3?dl=1")
 
 
 # helper functions to handle transformations
